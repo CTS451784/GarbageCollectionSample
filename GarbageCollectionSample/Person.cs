307fd8d117685f1f6163a745d771ce949c3217ee
cts451784 <Priya.Menon@cognizant.com>
2015-10-09 14:38:13 +0530

    Garbage Collection
    Assignment for garbage collection

---
+++ GarbageCollectionSample/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GarbageCollectionSample
+{
+	public class Person
+	{
+		int[] age = new int[1000000];
+		string[] name = new string[1000000];
+
+	}
+	public class Employee : Person
+	{
+		int[] empid = new int[1000000];
+	}
+
+
+
+
+}
+
---
+++ GarbageCollectionSample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GarbageCollectionSample
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			newEmployee (1000000);
+			Console.WriteLine ("Memory used before collection" + GC.GetTotalMemory (false));
+
+			GC.Collect ();
+			Console.WriteLine ("Memory used after collection " + GC.GetTotalMemory (false));
+
+		 //*****////
+			long val1 = GC.GetTotalMemory (false); // Not to wait for garbage collector 
+			{
+				int[] values = new int[1000000];
+				values = null; // Allocating an Array and making itar NotSupportedException reachable
+			}
+			long val2 = GC.GetTotalMemory (false);
+			{
+				GC.Collect (); //gets immediate garbage
+			}
+			long val3 = GC.GetTotalMemory (false);
+			{
+				Console.WriteLine ("Sample Array checking");
+				Console.WriteLine (val1);
+				Console.WriteLine (val2);
+				Console.WriteLine (val3);
+
+
+			}
+
+			/*****///
+			Console.ReadLine ();
+		}
+		static void newEmployee(int size)
+		{
+			Employee e;
+			for (int i = 0; i < size; i++) {
+
+				e = new Employee ();
+			}
+
+		}
+	}
+}
